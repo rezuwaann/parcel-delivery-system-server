a1adec615cbf01b18ad752325a8298ce2e13aa9f
Rezuwan Ahmed <rezuwaann@Rezuwans-MacBook-Air.local>
2026-08-08 02:47:28 +0600

    stripe payment method added

--- index.js
+++ index.js
@@ -1,14 +1,133 @@
 const express = require('express');
-const cors=require('cors')
+const cors = require('cors')
 const app = express()
 const port = process.env.PORT || 3000;
 require('dotenv').config();
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+
+// stripe
+const stripe = require('stripe')
+    (process.env.STRIPE_SECRET);
+
 
 
 
 // const middleware
 app.use(express.json())
 app.use(cors())
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.atbpap4.mongodb.net/?appName=Cluster0`;
+
+
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+
+        const db = client.db("delivery_system_db");
+        const parcelsCollection = db.collection('parcels');
+
+
+
+        // parcel api
+        app.get('/parcels', async (req, res) => {
+            const query = {};
+
+            const { email } = req.query;
+            if (email) {
+                query.senderEmail = email
+            }
+
+
+            const cursor = parcelsCollection.find(query).sort({ createdAt: -1 });
+            const result = await cursor.toArray();
+            res.send(result)
+
+        })
+
+        app.get('/parcels/:id', async (req, res) => {
+
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+
+            const result = await parcelsCollection.findOne(query);
+            res.send(result);
+
+
+        })
+
+        app.post('/parcels', async (req, res) => {
+            const parcel = req.body;
+            parcel.createdAt = new Date();
+
+            const result = await parcelsCollection.insertOne(parcel);
+            res.send(result)
+        })
+
+
+        app.delete('/parcels/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+
+            const result = await parcelsCollection.deleteOne(query);
+            res.send(result);
+        })
+
+
+
+
+        // payment related apis
+        app.post('/create-checkout-session', async (req, res) => {
+            const paymentInfo = req.body;
+            const amount = parseInt((paymentInfo.cost) * 100)
+            const session = await stripe.checkout.sessions.create({
+                line_items: [
+                    {
+                        // Provide the exact Price ID (for example, price_1234) of the product you want to sell
+                        price_data: {
+                            currency: 'BDT',
+                            unit_amount: amount,
+                            product_data: {
+                                name: paymentInfo.parcelName
+                            }
+                        },
+                        quantity: 1,
+                    },
+                ],
+                customer_email: paymentInfo.senderEmail,
+                mode: 'payment',
+                metadata: { parcelId: paymentInfo.parcelId },
+                success_url: `${process.env.SITE_DOMAIN}/dashboard/payment-success?session_id={CHECKOUT_SESSION_ID}`,
+                cancel_url: `${process.env.SITE_DOMAIN}/dashboard/payment-cancelled`,
+                // Provide a name (for example, hosted_web_0001) to label this Checkout integration and measure its conversion independently
+                // integration_identifier: '{{INTEGRATION_ID}}',
+            });
+
+            console.log(session)
+            res.send({ url: session.url })
+        })
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
 
 
 
